a70eb18af06fdef870e1aff12f5b0a89a0c7261e
Luigi Di Benedetto <ing.luigidibenedetto@gmail.com>
2021-02-24 15:46:12 +0100

    button task done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+function printInConsole() {
+  const consola = require('consola')
+  return consola.info('Hello, this is my first react app!')
+}
+
 function App() { 
   return (
     <div className="App">
@@ -20,10 +25,12 @@ function App() {
       </header> 
       <main className="App-main">
         <h1 className="App-main-h1">My First React App</h1>
+
+        <button onClick={printInConsole}>Print in console</button>
+
       </main>
       <footer className="App-footer">copyright by Me {(new Date().getFullYear())}</footer>
     </div>
   );
 }
-
 export default App;
