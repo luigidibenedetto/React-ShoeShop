43ebb1a526b7fce6ae6a281576f62c59519ee379
Gionatan Lombardi <gionatan.lombardi@musement.com>
2021-02-25 13:05:00 +0100

    feat: added LogoImg and Product components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import LogoImg from './components/LogoImg';
+import Product from './components/Product'
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <LogoImg alternativeText="React Header Logo" imageSrc={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +21,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <ul>
+         <Product name="Red Shoe" price={23.5} />
+         <Product name="Blue Shoe" price={98.3} />
+         <Product name="Black Shoe" price={52.7} />
+        </ul>
+      </main>
+      <footer><LogoImg alternativeText="Edgemony Footer Logo" imageSrc="https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png" /></footer>
     </div>
   );
 }
